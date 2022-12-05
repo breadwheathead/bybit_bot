f6c8ffc239612e5f908a5a26920b8e7f85e3fb46
Bread <breadwheatbox@gmail.com>
2022-12-05 20:25:57 +0700

    соединение установлено, ответ от bybit API получен

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
-import pybit
+import logging
+import aiogram
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from pybit import inverse_perpetual
+
+logging.basicConfig(level=logging.INFO)
+
+session = inverse_perpetual.HTTP(endpoint='https://api.bybit.com')
+
+print(session.latest_information_for_symbol(symbol='SOLUSDT'))
 
 
 if __name__ == '__main__':
