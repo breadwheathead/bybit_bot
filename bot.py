f60294c70b5d3eb8f139237ed5b2bd93e158c537
Bread <breadwheatbox@gmail.com>
2022-12-05 20:45:32 +0700

    создан aiogram handler start

--- bot.py
+++ bot.py
@@ -4,13 +4,23 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from pybit import inverse_perpetual
+from config.reader import load_config
 
 logging.basicConfig(level=logging.INFO)
 
 session = inverse_perpetual.HTTP(endpoint='https://api.bybit.com')
 
-print(session.latest_information_for_symbol(symbol='SOLUSDT'))
+config = load_config('config/bot.ini')
+bot = Bot(token=config.tg_bot.token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Hi')
+
+# print(session.latest_information_for_symbol(symbol='SOLUSDT'))
 
 
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp, skip_updates=True)
